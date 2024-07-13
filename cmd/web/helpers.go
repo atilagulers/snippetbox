dca7753be49ec20350206d8457cc7e1eff35c15d
Atila Guler <atilagulerdev@gmail.com>
2024-07-13 16:26:56 +0300

    add latest snippet

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"net/http"
-	"runtime/debug"
 )
 
 func (app *application) serverError(w http.ResponseWriter, r *http.Request, err error) {
@@ -11,9 +10,7 @@ func (app *application) serverError(w http.ResponseWriter, r *http.Request, err 
 		uri    = r.URL.RequestURI()
 	)
 
-	trace := string(debug.Stack())
-
-	app.logger.Error(err.Error(), "method", method, "uri", uri, "trace", trace)
+	app.logger.Error(err.Error(), "method", method, "uri", uri)
 
 	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 }
